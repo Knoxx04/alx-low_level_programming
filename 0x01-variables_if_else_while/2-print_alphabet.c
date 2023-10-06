be4e8e95615ed1b7753e1de82289a42c382d2331
Knoxx04 <onyiknoxx3@gmail.com>
2023-10-06 12:56:37 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+    char start = 'a';
+    char end = 'z';
+
+    for (; start <= end; start++);
+    {
+        putchar(start);
+	putchar('\n');
+    }
+    return (0);
+}
