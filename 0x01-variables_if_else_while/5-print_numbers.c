b4c82faacfcf2a85ba2dec01cf5ca9847f20a124
Knoxx04 <onyiknoxx3@gmail.com>
2023-10-06 16:38:18 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print single digit base 10 numbers starting from 0
+ *
+ * Return: Always 0 (succes)
+ */
+int main(void)
+{
+	int num;
+
+	for(num = 0; num < 10; num++)
+	{
+		printf(num);
+	}
+	return (0);
+}
