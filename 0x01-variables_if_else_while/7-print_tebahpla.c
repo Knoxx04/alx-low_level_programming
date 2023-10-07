dcd0f459d6f69c24e3c64f237157b401c0d1e70a
Knoxx04 <onyiknoxx3@gmail.com>
2023-10-06 17:45:00 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print numbers in reverse
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+    char start = 'a';
+    char end = 'z';
+
+    for (; end >= start; end--)
+    {
+        putchar(end);
+    }
+    putchar('\n');
+    return (0);
+}
