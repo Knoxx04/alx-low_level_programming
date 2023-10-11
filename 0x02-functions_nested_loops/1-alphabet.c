b9f61c16435c690236abcdf18c46bd924675b667
Knoxx04 <onyiknoxx3@gmail.com>
2023-10-11 09:47:50 -0700

    1-alphabet.c-11

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,7 @@
 /**
  * main - Entry point
  *
- * Description: Print the alphabet in lowercase followed by a newline
+ * print_alphabet: Print the alphabet in lowercase followed by a newline
  *
  * Return: void
  */
