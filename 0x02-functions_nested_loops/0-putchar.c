2d89338b633d512c708194342333ed8076dd0a40
Knoxx04 <onyiknoxx3@gmail.com>
2023-10-11 05:24:44 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 
+ * Description: Function that prints putchar followed by a new line
+ *
+ * Return: Always 0 (success)
+ */
+function main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
