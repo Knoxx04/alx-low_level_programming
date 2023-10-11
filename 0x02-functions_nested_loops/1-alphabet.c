ef5f8eb37e33c7d0e3bd6e569db2add2bb305ac6
Knoxx04 <onyiknoxx3@gmail.com>
2023-10-11 06:09:14 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Description: Print the alphabet in lowercase followed by a newline
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char min = '97'
+
+		while (min <= '122')
+		{
+			min++
+		}
+	_putchar('\n')
+
+		return (0);
+}
