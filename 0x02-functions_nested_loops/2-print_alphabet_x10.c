c307bec73ad20f7053b20ebe3075c48c742a223f
Knoxx04 <onyiknoxx3@gmail.com>
2023-10-11 10:20:42 -0700

    2-print_alphabet_x10.c-2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,11 +6,15 @@
  */
 void print_alphabet_x10(void)
 {
-	char c[] = "abcdefghijklmnopqrstuvwxyz'\n'";
+    int i;
+    char c;
 
-	for (c = 0; c <= 10; c++)
-	{
-		_putchar(c);
-	}
-	_putchar('\n');
+    for (i = 0; i < 10; i++)
+    {
+        for (c = 'a'; c <= 'z'; c++)
+        {
+            _putchar(c);
+        }
+        _putchar('\n');
+    }
 }
